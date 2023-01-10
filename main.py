14ed66fbc3f9eb10b528f0a55ca8d57961894795
Cancerous115 <107626947+Cancerous115@users.noreply.github.com>
2023-01-10 01:30:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+character = input(" Which Marvel Character are you?")
+myFood = input("What's your favorite fruit?")
+if myFood == "Strawberrys":
+   print("so not antman")
+else:
+  print("so not blade")
+myPower = input("What power would you want?")
+if myPower == "Flight":
+  print("so not antman")
+else:
+  print("so not Fing fang foom")
+myColor = input(" What's your favorite Color?")
+if myColor == "red":
+  print("So youre not hawkeye!")
+else:
+  print("so not she hulk")
+print()
+character = input(" Do you like hanging around?")
+if character == "yes":
+  print("You're the Scarlet Spider")
+else:
+  print("Finally, the character you are is the punisher!!")
